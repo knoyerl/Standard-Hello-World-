b8a03d96e1f3a0d5318bae3f04eea16118200211
knoyerl <48767801+knoyerl@users.noreply.github.com>
2019-07-20 11:15:58 -0400

    Add files via upload

---
+++ Coding Projects/Course Work Project 2/Course2.js
@@ -0,0 +1,40 @@
+function courseOne() {
+	alert("Math 101 \n Start Time: 7:00am");
+	} 
+
+function courseTwo() {
+	alert("Science 101 \n Start Time: 8:00am");
+	} 
+
+function courseThree() {
+	alert("History 101 \n Start Time: 9:00am");
+	} 
+
+function courseFour() {
+	alert("Geography 101 \n Start Time: 10:00am");
+	} 
+
+function courseFive() {
+	alert("Woodworking 101 \n Start Time: 11:00am");
+	} 
+
+function courseSix() {
+	alert("Coding 101 \n Start Time: 12:00pm");
+	} 
+
+function courseSeven() {
+	alert("Networking 101 \n Start Time: 1:00pm");
+	} 
+
+function courseEight() {
+	alert("Technology 101 \n Start Time: 2:00pm");
+	} 
+
+function courseNine() {
+	alert("Art 101 \n Start Time: 3:00pm");
+	} 
+
+function courseTen() {
+	alert("Graphic Design 101 \n Start Time: 4:00pm");
+	} 
+
